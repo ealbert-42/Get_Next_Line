a49493330e416ea7a748cdd6ac791cebc5c99121
Eddy ALBERT <ealbert@e1r8p10.42.fr>
2016-04-04 20:52:59 +0200

    YEAH

--- get_next_line.c
+++ get_next_line.c
@@ -1,99 +1,109 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   new.c                                              :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ealbert <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/03/17 12:21:44 by ealbert           #+#    #+#             */
-/*   Updated: 2016/03/17 17:52:46 by ealbert          ###   ########.fr       */
+/*   Created: 2016/03/17 11:47:30 by ealbert           #+#    #+#             */
+/*   Updated: 2016/04/04 20:50:36 by ealbert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
-#include <fcntl.h>
+//#include "get_next_line.h"
+#define MAX_FD 256
+#define BUFF_SIZE 3
+#include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "libft.h"
 
-static char			*joining(char *s1, char *s2)
+int			find_nl(char *str)
 {
-	char			*new;
-
-	if (!s1)
-	{
-		if (!(new = malloc(ft_strlen(s2) + 1)))
-			return (NULL);
-		ft_strcpy(new, s2);
-		return (new);
-	}
-	else
-	{
-		if (!(new = malloc(ft_strlen(s1) + ft_strlen(s2) + 1)))
-			return (NULL);
-		ft_strcpy(new, s1);
-		ft_strcat(new, s2);
-		return (new);
-	}
-}
-
-static char			*allocation(char *str)
-{
-	int				i;
-	int				j;
-
-	i = 1;
-	while (str[i - 1] != '\n' && str[i - 1] != '\0')
-		i++;
-	j = i;
-	while (str[j] != '\0')
-		j++;
-	if (j - i == 0)
-		return (NULL);
-	return (ft_strsub(str, i, j - i));
-}
-
-static char			*cutting(char *str)
-{
-	int				i;
-	char			*tmp;
+	int		i;
 
 	i = 0;
-	if (!str)
-		return (NULL);
-	while (str[i] != '\n' && str[i] != '\0')
-		i++;
-	tmp = malloc(i + 1);
-	i = 0;
-	while (str[i] != '\n' && str[i] != '\0')
+	while (str[i])
 	{
-		tmp[i] = str[i];
+		if (str[i] == '\n')
+				return (i);
 		i++;
 	}
-	tmp[i] = '\0';
-	return (tmp);
+	return (-1);
 }
 
-int					get_next_line(int fd, char **line)
+int			cut_line(char **array, char **line, int len)
+{
+	char	*tmp;
+
+	*line = ft_strsub(*array, 0, len);
+	tmp = *array;
+	*array = ft_strdup(ft_strchr(*array, '\n') + 1);
+	ft_strdel(&tmp);
+	printf("\narray ===>'%s'\n", *array);
+	return (1);
+}
+
+int			read_file(int fd, char **array, char **line)
+{
+	char	buf[BUFF_SIZE + 1];
+	int		ret;
+	char	*tmp;
+	int		len;
+
+	tmp = NULL;
+	while ((ret = read(fd, buf, BUFF_SIZE)))
+	{
+printf("%d <= ret\n", ret);
+printf("array =>'%s'\n", *array);
+		if (ret == -1)
+			return (-1);
+		buf[ret] = '\0';
+		if ((len = find_nl(*array)) > -1)
+			return (cut_line(array, line, len));
+		tmp = *array;
+		*array = ft_strjoin(*array, buf);
+		ft_strdel(&tmp);
+	}
+	return (1);
+}
+
+int			get_next_line(int fd, char **line)
 {
 	static char		*array[MAX_FD];
-	unsigned int	ret;
-	char			buf[BUFF_SIZE + 1];
+	int				ret;
+	int				len;
 
-	if (fd < 3 || fd > MAX_FD || !(*line = malloc(1)))
+	*line = "";
+	if (fd < 3 || fd > MAX_FD || BUFF_SIZE <= 0)
 		return (-1);
-	ret = BUFF_SIZE;
-	if (!array[fd - 3])
+//	if ((len = find_nl(array[fd])) != -1)
+//		return (cut_line(&array[fd], line, len));
+	if (!array[fd])
+		array[fd] = ft_strnew(0);
+	if ((ret = read_file(fd, &array[fd], line)) && ret != 2)
+		return (ret);
+	return (0);
+}
+
+int			main(int argc, char *argv[])
+{
+	int		fd;
+	char	*line;
+	int		get;
+	int		ok;
+
+	ok = 0;
+	get = 1;
+	line = ft_strnew(0);
+	fd = open(argv[1], O_RDONLY);
+	while (get)
 	{
-		while (ret == BUFF_SIZE)
-		{
-			if ((ret = read(fd, &buf, BUFF_SIZE)) < 1)
-				return (ret);
-			buf[ret] = '\0';
-			array[fd - 3] = joining(array[fd - 3], buf);
-		}
+		get = get_next_line(fd, &line);
+		if (get == -1 || ok == 1)
+			break ;
+		printf("====>'%s'<====\nget = %d\n", line, get);
 	}
-	*line = cutting(array[fd - 3]);
-	array[fd - 3] = allocation(array[fd - 3]);
-	if (*line)
-		return (1);
 	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: ealbert <ealbert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/12 15:00:41 by ealbert           #+#    #+#             */
-/*   Updated: 2016/03/17 17:52:35 by ealbert          ###   ########.fr       */
+/*   Updated: 2016/04/04 17:06:14 by ealbert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 50
-# define MAX_FD 256
+# define BUFF_SIZE 1
+# define MAX_FD 1000
 # include "libft.h"
 
 int					get_next_line(int fd, char **line);
