9cccac762fb4eeb10b5a693776db8c09ecf9e699
Eddy Albert <ealbert@student.42.fr>
2016-04-08 18:07:15 +0200

    Fresh

---
+++ get_next_line.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ealbert <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/04/08 17:55:35 by ealbert           #+#    #+#             */
+/*   Updated: 2016/04/08 18:02:09 by ealbert          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
+# include "libft.h"
+int		get_next_line(int fd, char **line);
+#endif
